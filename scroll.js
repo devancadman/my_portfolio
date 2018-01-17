3d40517519a1b8f90a1e3538262774cb4cf85718
Devman <devanstardis@gmail.com>
2018-01-17 14:02:54 +0000

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,2 +1,26 @@
 $('body').attr('data-spy', 'scroll');
   $('body').attr('data-target', '#navbar-collapse-scroll');
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 10px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0 }, 100);              // Scroll to top of body   
+});
+
+/* Copyright Year   */
+let date = new Date().getFullYear();
+document.getElementById('currentyear').innerHTML = date;
+
+/* Character Length */
+var maxLength = 100;
+$('textarea').keyup(function() {
+  var length = $(this).val().length;
+  var length = maxLength-length;
+  $('#chars').text(length);
+});
