7f519196194bf9e75afe66d718a07c832b6fdc3d
Devman <devanstardis@gmail.com>
2018-02-07 01:41:15 +0000

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -21,6 +21,5 @@ document.getElementById('currentyear').innerHTML = date;
 var maxLength = 100;
 $('textarea').keyup(function() {
   var length = $(this).val().length;
-  var length = maxLength-length;
   $('#chars').text(length);
 });
