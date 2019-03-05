6fd75d623331d20bb79b89ffc27813382d3de3f5
Devman <devanstardis@gmail.com>
2019-03-05 23:21:14 +0000

    Incorporated a Smooth Scroll to Nav Links
    // ===== Smooth Scroll ==== 
    $(function() {
      $('a[href*="#"]:not([href="#"])').click(function() {
        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
          var target = $(this.hash);
          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
          if (target.length) {
            $('html, body').animate({
              scrollTop: target.offset().top
            }, 1000);
            return false;
          }
        }
      });
    })

--- scroll.js
+++ scroll.js
@@ -1,5 +1,22 @@
 $('body').attr('data-spy', 'scroll');
   $('body').attr('data-target', '#navbar-collapse-scroll');
+
+// ===== Smooth Scroll ==== 
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+})
+
 // ===== Scroll to Top ==== 
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 10px
@@ -13,11 +30,11 @@ $('#return-to-top').click(function() {      // When arrow is clicked
         scrollTop : 0 }, 100);              // Scroll to top of body   
 });
 
-/* Copyright Year   */
+// ===== Copyright Year =====
 let date = new Date().getFullYear();
 document.getElementById('currentyear').innerHTML = date;
 
-/* Character Length */
+// ===== Character Length =====
 var maxLength = 100;
 $('textarea').keyup(function() {
   var length = $(this).val().length;
